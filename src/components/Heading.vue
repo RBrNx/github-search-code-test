444cafa4607ffb76e2e55904668e854382a06090
RBrNx <conor.watson94@gmail.com>
2018-10-09 21:02:57 +0100

    Created Heading.vue and Results.vue. Added Title, Subtitle and Searchbox to Heading.vue. Removed some default Vue files. Added global.scss.

---
+++ src/components/Heading.vue
@@ -0,0 +1,84 @@
+<template>
+	<div id="Heading">
+		<h2 class="title">Github Repository Search</h2>
+		<h4 class="subtitle">Start typing to search for a Github Repository!</h4>
+		<div class="searchContainer">
+			<input type="text" id="search" placeholder="What repository are you looking for..." v-model="query">
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'Heading',
+	components: {
+
+	},
+	props: [
+
+	],
+	methods: {
+
+	},
+	computed: {
+
+	},
+	data(){
+		return {
+			query: "",
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/global.scss";
+
+	#Heading{
+		padding: 0 22%;
+		padding-top: 20%;
+        font-family: 'Open Sans', sans-serif;
+        
+		.title{
+			text-align: center;
+			margin: 0;
+			padding: 10px 20px;
+			font-family: 'Roboto Condensed', sans-serif;
+			font-size: 65px;
+		}
+
+		.subtitle{
+			text-align: center;
+			margin: 0;
+			padding: 10px 20px;
+		}
+
+		.searchContainer{
+			margin-top: 40px;
+
+			#search{
+				text-align: center;
+				width: 90%;
+				height: 50px;
+				font-size: 20px;
+				color: $regularText;
+				border: 5px solid #132c52;
+				box-sizing: border-box;
+				display: inline-block;
+				border-radius: 5px 0px 0px 5px;
+
+				&:focus{
+					outline: none;
+				}
+
+				&::placeholder{
+					opacity: 1;
+					color: lightgrey;
+					font-size: 16px;
+				}
+			}
+		}
+	}
+</style>
+`
---
+++ src/components/Results.vue
@@ -0,0 +1,42 @@
+<template>
+	<div id="Results">
+        <span>Results go here</span>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'Results',
+	components: {
+
+    },
+    props: [
+        "results",
+    ],
+    methods: {
+
+    },
+    watch: {
+
+    },
+	data(){
+		return {
+            searchResults: [],
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/global.scss";
+
+	#Results{
+        margin-top: 50px;
+        padding: 0 15%;
+        padding-bottom: 20px;
+        font-family: 'Open Sans', sans-serif;
+        position: relative;
+	}
+</style>
+`
